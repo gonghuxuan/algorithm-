74cf20ab639d189645ca2414ed80014869f9a7c5
gonghuxuan <741247407@qq.com>
2022-12-15 19:39:41 +0800

    promise a+

--- 手写/promise.js
+++ 手写/promise.js
@@ -8,6 +8,12 @@
 
 //  5.添加then（参数，处理参数）
 
+//  6.then返回一个新的promise，将状态为pending存入下一个处理的数组中
+
+//  7.对onFulfilled函数和onRjected函数进行try  catch错误处理，错误的话将状态设为reject，正确的话使用resolvePromise设置promise2状态
+
+//  8.resolvePromise判断几种条件（promise2和x相等，x为promsie的实力，x是其他类型的promise，x是其他）
+
 class A {
     constructor(fun) {
         this.fun = fun
