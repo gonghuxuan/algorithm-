577cd06478794be4641388dbeb5eec57a3c80bfa
gonghuxuan <741247407@qq.com>
2023-02-20 14:56:10 +0800

    add .

---
+++ test.js
@@ -0,0 +1,5 @@
+function numberToMoney(num) {
+    return  Number(num).toLocaleString()
+}
+
+console.log(numberToMoney('1232131232'))
--- 有用的业务代码/小功能合集.js
+++ 有用的业务代码/小功能合集.js
@@ -1,3 +1,13 @@
+/**
+ * 
+ * 目录
+ * 
+ * 1.监听其他标签页的更新通过storage
+ * 2.随机颜色
+ * 3.数字转金额
+ */
+
+
 // 监听其他标签页的更新通过storage
 
 window.addEventListener('storage', (e) => {
@@ -7,3 +17,27 @@ window.addEventListener('storage', (e) => {
 //  css变量对于换肤的实现
 
 //  减少使用background类似的复合属性，复合属性会覆盖之前的一些属性。
+
+//  随机颜色
+function getRandomColor() {
+    let flag = true
+    let colorStr
+    while(flag) {
+        colorStr = Math.random().toString(16)
+        if(colorStr.length >= 8) {
+            flag = false
+        }
+    }
+    return '#' + colorStr.substring(2,8)
+}
+
+//  数字转金额
+function numberToMoney(num) {
+    if(typeof num !== 'string')
+    num = num.toString()
+    return  num.replace(/(?=\B(\d{3})+$)/g, ',')
+}
+
+function numberToMoney(num) {
+    return  Number(num).toLocaleString()
+}
